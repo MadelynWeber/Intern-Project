64fee4567adac2c0e26a9dbdfd82dab7753d75fe
Angelina <angzheng@umich.edu>
2022-07-12 16:23:05 -0500

    cleaned reviews

---
+++ sentiment-analysis/classifier.py
@@ -0,0 +1,19 @@
+"Reads in contractor review data from web scraping code and classifies reviews on a 1-5 star scale"
+from io import StringIO
+import pandas as pd
+
+def main():
+    df = pd.read_csv ('cleaned_reviews.csv')
+    col = ['Rating', 'Review Text']
+    df = df[col]
+    # df = df[pd.notnull(df['Consumer complaint narrative'])]
+    # df.columns = ['Product', 'Consumer_complaint_narrative']
+    # df['category_id'] = df['Product'].factorize()[0]
+    # category_id_df = df[['Product', 'category_id']].drop_duplicates().sort_values('category_id')
+    # category_to_id = dict(category_id_df.values)
+    # id_to_category = dict(category_id_df[['category_id', 'Product']].values)
+    # df.head()
+    
+
+if __name__ == "__main__":
+    main()
