0d1dfb719ad49efe345cfdecb24f1d1a6e694837
Angelina <angzheng@umich.edu>
2022-07-14 16:08:28 -0500

    completed training on review text using vector representations using tf-idf weighted vecttors, most correlated words for 5-STAR category however does not make sense

--- sentiment-analysis/classifier.py
+++ sentiment-analysis/classifier.py
@@ -1,8 +1,13 @@
 "Reads in contractor review data from web scraping code and classifies reviews on a 1-5 star scale"
 from io import StringIO
 import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_selection import chi2
+import numpy as np
+
 
 def main():
+    # ------------------------------------------------CLEAN REVIEWS FOR ANALYSIS ---------------------------------------------------
     df = pd.read_csv ('review_data.csv')
     # following lines of code drops duplicate reviews and any rows with empty values
     df.drop_duplicates(inplace = True)
@@ -11,17 +16,27 @@ def main():
     cleaned_df = df[df['ReviewText'].str.contains('[A-Za-z0-9]')]
     # temp.to_csv("temp.csv", index=False)
     cleaned_df.to_csv('cleaned_reviews.csv', index=False)
+
+    # ------------------------------------------------ TEST REVIEWS ON ANALYSIS MODELS ---------------------------------------------------
     df = pd.read_csv ('cleaned_reviews.csv')
-    print(df)
-    # col = ['Rating', 'Review Text']
-    # df = df[col]
-    # df = df[pd.notnull(df['Consumer complaint narrative'])]
-    # df.columns = ['Product', 'Consumer_complaint_narrative']
-    # df['category_id'] = df['Product'].factorize()[0]
-    # category_id_df = df[['Product', 'category_id']].drop_duplicates().sort_values('category_id')
-    # category_to_id = dict(category_id_df.values)
-    # id_to_category = dict(category_id_df[['category_id', 'Product']].values)
-    # df.head()
+    df.head()
+    tfidf = TfidfVectorizer(sublinear_tf=True, min_df=5, norm='l2', encoding='latin-1', ngram_range=(1, 3))
+    features = tfidf.fit_transform(df.ReviewText).toarray()
+    labels = df.Rating
+    Ratings = [1, 2, 3, 4, 5]
+    features.shape
+    N = 2
+    for category_id in Ratings:
+        features_chi2 = chi2(features, labels == category_id)
+        indices = np.argsort(features_chi2[0])
+        feature_names = np.array(tfidf.get_feature_names_out())[indices]
+        unigrams = [v for v in feature_names if len(v.split(' ')) == 1]
+        bigrams = [v for v in feature_names if len(v.split(' ')) == 2]
+        trigrams = [v for v in feature_names if len(v.split(' ')) == 3]
+        print("# '{}':".format(category_id))
+        print("  . Most correlated unigrams:\n. {}".format('\n. '.join(unigrams[-N:])))
+        print("  . Most correlated bigrams:\n. {}".format('\n. '.join(bigrams[-N:])))
+        print("  . Most correlated trigrams:\n. {}".format('\n. '.join(trigrams[-N:])))
     
 
 if __name__ == "__main__":
