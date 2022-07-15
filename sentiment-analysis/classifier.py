7f443656fd6dbc2a60c1bea43756d7fa751f794b
Angelina <angzheng@umich.edu>
2022-07-15 12:04:28 -0500

    completed model analysis and tests; will need to look into discrepencies and possible areas for improvement

--- sentiment-analysis/classifier.py
+++ sentiment-analysis/classifier.py
@@ -2,8 +2,18 @@
 from io import StringIO
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import LinearSVC
+from sklearn.model_selection import cross_val_score
 from sklearn.feature_selection import chi2
+import matplotlib.pyplot as plt
 import numpy as np
+import seaborn as sns
 
 # ------------------------------------------------CLEAN REVIEWS FOR ANALYSIS ---------------------------------------------------
 def cleanReviews():
@@ -24,7 +34,7 @@ def main():
     tfidf = TfidfVectorizer(sublinear_tf=True, min_df=5, norm='l2', encoding='latin-1', ngram_range=(1, 3))
     features = tfidf.fit_transform(df.ReviewText).toarray()
     labels = df.Rating
-    Ratings = [1, 2, 3, 4, 5]
+    Ratings = [1, 2, 3, 4, 5, ]
     features.shape
     N = 5
     for category_id in Ratings:
@@ -38,7 +48,52 @@ def main():
         print("  . Most correlated unigrams:\n. {}".format('\n. '.join(unigrams[-N:])))
         print("  . Most correlated bigrams:\n. {}".format('\n. '.join(bigrams[-N:])))
         print("  . Most correlated trigrams:\n. {}".format('\n. '.join(trigrams[-N:])))
+    # ------------------------------------------------ NAIVE BAYES CLASSIFIER  ---------------------------------------------------
+
+    X_train, X_test, y_train, y_test = train_test_split(df['ReviewText'], df['Rating'], random_state = 0)
+    count_vect = CountVectorizer()
+    X_train_counts = count_vect.fit_transform(X_train)
+    tfidf_transformer = TfidfTransformer()
+    X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
+    clf = MultinomialNB().fit(X_train_tfidf, y_train)
+
+    # ------------------------------------------COMPARE DIFFERING MACHINE LEARNING MODELS  ------------------------------------------------
+    models = [
+        RandomForestClassifier(n_estimators=200, max_depth=3, random_state=0),
+        LinearSVC(),
+        MultinomialNB(),
+        LogisticRegression(random_state=0),
+    ]
+    CV = 5
+    cv_df = pd.DataFrame(index=range(CV * len(models)))
+    entries = []
+    for model in models:
+        model_name = model.__class__.__name__
+        accuracies = cross_val_score(model, features, labels, scoring='accuracy', cv=CV)
+        for fold_idx, accuracy in enumerate(accuracies):
+            entries.append((model_name, fold_idx, accuracy))
+    cv_df = pd.DataFrame(entries, columns=['model_name', 'fold_idx', 'accuracy'])
     
+    sns.boxplot(x='model_name', y='accuracy', data=cv_df)
+    sns.stripplot(x='model_name', y='accuracy', data=cv_df, 
+                size=8, jitter=True, edgecolor="gray", linewidth=2)
+    plt.show()
+    print(cv_df.groupby('model_name').accuracy.mean())
+
+    # ------------------------------------------ MODEL EVALUATION  ------------------------------------------------
+    model = LinearSVC()
+    X_train, X_test, y_train, y_test, indices_train, indices_test = train_test_split(features, labels, df.index, test_size=0.33, random_state=0)
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    from sklearn.metrics import confusion_matrix
+    conf_mat = confusion_matrix(y_test, y_pred)
+    fig, ax = plt.subplots(figsize=(10,10))
+    sns.heatmap(conf_mat, annot=True, fmt='d',
+                xticklabels=Ratings, yticklabels=Ratings)
+    plt.ylabel('Actual')
+    plt.xlabel('Predicted')
+    plt.show()
+
 
 if __name__ == "__main__":
     main()
